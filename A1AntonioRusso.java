c0b5cf24e3da00491c4c90fefa16fc90322c2e6e
Antonio Russo <antonio.cror@gmail.com>
2023-05-10 19:39:26 -0300

    First Commit - Class Creation

---
+++ A1AntonioRusso.java
@@ -0,0 +1,13 @@
+/*
+ * Código Fonte da Prova A1 de Programação de Soluções Computacionais.
+ * Nome do Aluno: Antonio Carlos Rodrigo de Oliveira Russo
+ * RA: 12523118842
+ * Caminho do Repositório: https://github.com/antoniocrorusso/A1_AntonioRusso.git
+*/
+
+public class A1AntonioRusso{
+    public static void main(String[] args) {
+        
+    }
+
+}
